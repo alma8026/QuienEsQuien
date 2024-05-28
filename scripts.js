8d4ec4dc63d5e52bb47f10c8d720b555d2ef9999
alma8026 <alikswtf@gmail.com>
2024-05-28 15:58:25 +0200

    plantilla con funcionalidades en el script

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+/**
+ * Funcionalidades:
+ * Menu de inicio para elegir la dificultad:
+ * Facil: 3 minutos sin restar tiempo.
+ * Normal: 3 minutos restando 20 seg por pregunta.
+ * Dificil: 2 minutos restando 20 seg tmb.
+ * Botones con las preguntas.
+ * Temporizador de 3 min: con cada pregunta se resta 20 seg.
+ * Animacion de ganar o perder.
+ * Mejorar estilos.
+ */
+
 // scripts.js
 const characters = [
     { name: 'Alice', img: 'https://randomuser.me/api/portraits/women/1.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: false, piercings: false, tatuajes: false, pelolargo: true, gafas: false, bigote: false },
