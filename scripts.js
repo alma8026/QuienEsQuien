43c9b83291f73e3a6f4661b2a5704cb9a30a2007
alma8026 <alikswtf@gmail.com>
2024-05-28 19:37:40 +0200

    base primer dia

--- scripts.js
+++ scripts.js
@@ -1,66 +1,98 @@
 /**
  * Funcionalidades:
- * Menu de inicio para elegir la dificultad:
+ * 
  * Facil: 3 minutos sin restar tiempo.
  * Normal: 3 minutos restando 20 seg por pregunta.
  * Dificil: 2 minutos restando 20 seg tmb.
- * Botones con las preguntas.
+ * 
+ * Botones con las preguntas, al hacer clic se queda en rojo o en verde dependiendo de si es true o false.
+ * En cada persona saldrian dos botones de respuesta o descartar.
+ * Cuando se decarte un persona una X encima.
  * Temporizador de 3 min: con cada pregunta se resta 20 seg.
  * Animacion de ganar o perder.
  * Seleccion de persona random.
  * Mejorar estilos.
- * adhjasdj
+ * Curiosidad al acertar la persona.
+ * 
  */
 
 // scripts.js
 const characters = [
-    { name: 'Alice', img: 'https://randomuser.me/api/portraits/women/1.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: false, piercings: false, tatuajes: false, pelolargo: true, gafas: false, bigote: false },
-    { name: 'Bob', img: 'https://randomuser.me/api/portraits/men/1.jpg', rubio: false, moreno: true, pelirrojo: false, barba: true, sombrero: true, piercings: false, tatuajes: true, pelolargo: false, gafas: false, bigote: true },
-    { name: 'Charlie', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: false, pelirrojo: true, barba: false, sombrero: false, piercings: true, tatuajes: false, pelolargo: true, gafas: false, bigote: false },
-    { name: 'Diana', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: false, sombrero: true, piercings: false, tatuajes: true, pelolargo: true, gafas: true, bigote: false },
-    { name: 'Edward', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: true, sombrero: false, piercings: true, tatuajes: false, pelolargo: false, gafas: false, bigote: false },
-    { name: 'Fiona', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: false, sombrero: true, piercings: true, tatuajes: true, pelolargo: true, gafas: false, bigote: false },
-    { name: 'George', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: true, sombrero: false, piercings: false, tatuajes: false, pelolargo: false, gafas: true, bigote: false },
-    { name: 'Hannah', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: true, piercings: false, tatuajes: true, pelolargo: true, gafas: false, bigote: false },
-    { name: 'Ian', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: false, pelirrojo: true, barba: true, sombrero: false, piercings: true, tatuajes: false, pelolargo: false, gafas: true, bigote: false },
-    { name: 'Julia', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: false, piercings: false, tatuajes: true, pelolargo: true, gafas: false, bigote: false },
-    // Añadir 15 personajes más con diferentes atributos
-    { name: 'Kevin', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: false, sombrero: false, piercings: true, tatuajes: false, pelolargo: false, gafas: false, bigote: false },
-    { name: 'Laura', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: false, piercings: false, tatuajes: false, pelolargo: true, gafas: true, bigote: false },
-    { name: 'Mike', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: true, sombrero: true, piercings: false, tatuajes: true, pelolargo: false, gafas: false, bigote: true },
-    { name: 'Nina', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: false, pelirrojo: true, barba: false, sombrero: false, piercings: true, tatuajes: false, pelolargo: true, gafas: true, bigote: false },
-    { name: 'Oscar', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: true, sombrero: false, piercings: true, tatuajes: false, pelolargo: false, gafas: false, bigote: true },
-    { name: 'Paula', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: false, sombrero: true, piercings: false, tatuajes: true, pelolargo: true, gafas: true, bigote: false },
-    { name: 'Quentin', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: true, sombrero: false, piercings: false, tatuajes: false, pelolargo: false, gafas: false, bigote: true },
-    { name: 'Rachel', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: false, sombrero: true, piercings: true, tatuajes: true, pelolargo: true, gafas: false, bigote: false },
-    { name: 'Sam', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: true, sombrero: false, piercings: false, tatuajes: false, pelolargo: false, gafas: false, bigote: false },
-    { name: 'Tina', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: false, sombrero: true, piercings: true, tatuajes: true, pelolargo: true, gafas: true, bigote: false },
-    { name: 'Uma', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: false, piercings: true, tatuajes: false, pelolargo: true, gafas: false, bigote: false },
-    { name: 'Victor', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: true, sombrero: false, piercings: false, tatuajes: false, pelolargo: false, gafas: true, bigote: true },
-    { name: 'Wendy', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: true, piercings: false, tatuajes: true, pelolargo: true, gafas: false, bigote: false },
-    { name: 'Xavier', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: true, sombrero: false, piercings: true, tatuajes: false, pelolargo: false, gafas: true, bigote: false },
-    { name: 'Yara', img: 'https://i.imgur.com/abc123.jpg', rubio: true, moreno: false, pelirrojo: false, barba: false, sombrero: true, piercings: false, tatuajes: false, pelolargo: true, gafas: false, bigote: false },
-    { name: 'Zane', img: 'https://i.imgur.com/abc123.jpg', rubio: false, moreno: true, pelirrojo: false, barba: true, sombrero: false, piercings: true, tatuajes: false, pelolargo: false, gafas: false, bigote: true },
+    { name: 'Adan', img: 'QEQ_imgs/Adan-QuienEsQuien.jpeg', moreno: false, barba: false, gafas: false, mayor25: false, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: false, ojos_claros: false},
+    { name: 'Alejandro', img: 'QEQ_imgs/Alejandro-QuienEsQuien.jpeg', moreno: false, barba: false, gafas: false, mayor25: false, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: true, ojos_claros: true},
+    { name: 'Cesar', img: 'QEQ_imgs/Cesar-QuienEsQuien.jpg', moreno: true, barba: true, gafas: false, mayor25: true, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: false, ojos_claros: false},
+    { name: 'Cristian', img: 'QEQ_imgs/Cristian-QuienEsQuien.jpeg', moreno: true, barba: false, gafas: false, mayor25: true, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: false, ojos_claros: false},
+    { name: 'Desiree', img: 'QEQ_imgs/Desiree-QuienEsQuien.jpeg', moreno: false, barba: false, gafas: true, mayor25: true, pelo_largo: true, hombre: false, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: true, camiseta_clara: true, ojos_claros: true},
+    { name: 'Erik', img: 'QEQ_imgs/Erik-QuienEsQuien.jpeg', moreno: false, barba: true, gafas: false, mayor25: true, pelo_largo: false, hombre: true, barba_larga: true, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: true, ojos_claros: false},
+    { name: 'Fran', img: 'QEQ_imgs/Fran-QuienEsQuien.jpeg', moreno: false, barba: true, gafas: false, mayor25: false, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: true, ojos_claros: false},
+    { name: 'Gerard', img: 'QEQ_imgs/Gerard-QuienEsQuien.jpeg', moreno: false , barba: false , gafas: false , mayor25:false , pelo_largo: false , hombre: true , barba_larga: false , pelo_ondulado: true , pelo_suelto: true , pelo_rubio: false , camiseta_clara:true , ojos_claros:false  },
+    { name: 'Glenn', img: 'QEQ_imgs/Glenn-QuienEsQuien.jpeg', moreno: true , barba: false , gafas: false , mayor25: false, pelo_largo: false, hombre: true , barba_larga:false , pelo_ondulado:false , pelo_suelto:true , pelo_rubio: false, camiseta_clara:false , ojos_claros:false  },
+    { name: 'Irene', img: 'QEQ_imgs/Irene-QuienEsQuien.jpeg', moreno:false , barba: false , gafas:true , mayor25: true , pelo_largo: true, hombre: false, barba_larga:false , pelo_ondulado:true , pelo_suelto:true , pelo_rubio:false , camiseta_clara:true , ojos_claros:true  },
+    { name: 'Ivan', img: 'QEQ_imgs/Ivan-QuienEsQuien.jpeg', moreno: false , barba: true , gafas: true , mayor25:true , pelo_largo: false , hombre: true , barba_larga: true , pelo_ondulado: false , pelo_suelto: true , pelo_rubio: false , camiseta_clara:true , ojos_claros:false  },
+    { name: 'Josep', img: 'QEQ_imgs/Josep-QuienEsQuien.jpeg', moreno: false , barba: false , gafas: true , mayor25:false , pelo_largo: false , hombre: true , barba_larga: false , pelo_ondulado: false , pelo_suelto: true , pelo_rubio: true , camiseta_clara:true , ojos_claros:true  },
+    { name: 'Josias', img: 'QEQ_imgs/Josias-QuienEsQuien.jpeg',moreno: false , barba: true , gafas: false , mayor25:true , pelo_largo: false , hombre: true , barba_larga: true , pelo_ondulado: false , pelo_suelto: true , pelo_rubio: false , camiseta_clara:false , ojos_claros:false  },
+    { name: 'Jota', img: 'QEQ_imgs/Jota-QuienEsQuien.jpeg', moreno: false, barba: true , gafas: false, mayor25: true, pelo_largo: true, hombre: true, barba_larga: true, pelo_ondulado: true, pelo_suelto: false, pelo_rubio: false, camiseta_clara: true, ojos_claros: false },
+    { name: 'Marina', img: 'QEQ_imgs/Marina-QuienEsQuien.jpeg', moreno: false, barba: false , gafas: true, mayor25: true, pelo_largo: true, hombre: false, barba_larga: false, pelo_ondulado: false, pelo_suelto: false, pelo_rubio: false, camiseta_clara: true, ojos_claros: false },
+    { name: 'Nando', img: 'QEQ_imgs/Nando-QuienEsQuien.jpeg', moreno: false, barba: true , gafas: false, mayor25: true, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: false, pelo_rubio: false, camiseta_clara: true, ojos_claros: false },
+    { name: 'Sergio', img: 'QEQ_imgs/Sergio-QuienEsQuien.jpeg', moreno: false, barba: true , gafas: true, mayor25: true, pelo_largo: true, hombre: true, barba_larga: true, pelo_ondulado: true, pelo_suelto: false, pelo_rubio: false, camiseta_clara: false, ojos_claros: false },
+    { name: 'Solomon', img: 'QEQ_imgs/Solomon-QuienEsQuien.jpeg', moreno: false, barba: true , gafas: false, mayor25: false, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: true, ojos_claros: false },
+    { name: 'Yago', img: 'QEQ_imgs/Yago-QuienEsQuien.jpeg', moreno: false, barba: true , gafas: true, mayor25: false, pelo_largo: false, hombre: true, barba_larga: false, pelo_ondulado: false, pelo_suelto: true, pelo_rubio: false, camiseta_clara: true, ojos_claros: true },
 ];
-// prueba
-function generateBoard() {
-    const board = document.getElementById('board');
-    characters.forEach((character, index) => {     
-        const card = document.createElement('div');
-        card.classList.add('card');    
-        card.setAttribute('data-index', index);
-        card.innerHTML = `<img src="${character.img}" alt="${character.name}"><p>${character.name}</p>`;
-        board.appendChild(card);
-    });
+
+let difficulty;
+let selectedPerson;
+const timeLimit = getTimeLimit();
+let secondsLeft = timeLimit;
+
+function selectDifficulty(selectedDifficulty) {
+    difficulty = selectedDifficulty;
+    startGame();
+}
+
+function startGame() {
+    // Mostrar la sección de juego
+    document.querySelector('.game-container').style.display = 'block';
+    document.querySelector('.menu-container').style.display = 'none';
+
+    // Reiniciar el temporizador
+    resetTimer();
+}
+
+function resetTimer() {
+    let timer = setInterval(() => {
+        secondsLeft--;
+        document.getElementById('timer').textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timer);
+            // Aquí puedes agregar la lógica para cuando se acabe el tiempo
+            alert('¡Se acabó el tiempo!');
+        }
+    }, 1000);
+}
+
+function getTimeLimit() {
+    switch (difficulty) {
+        case 'easy':
+            return 61; // 60 segundos para la dificultad fácil
+        case 'normal':
+            return 46; // 45 segundos para la dificultad normal
+        case 'hard':
+            return 31; // 30 segundos para la dificultad difícil
+        default:
+            return 61; // Por defecto, la dificultad fácil
+    }
 }
 
 function askQuestion(attribute) {
-    characters.forEach((character, index) => {
-        const card = document.querySelector(`.card[data-index="${index}"]`);
-        if (!character[attribute]) {
-            card.classList.add('inactive');
-        }
-    });
+    secondsLeft -= 10;
+    
 }
 
-document.addEventListener('DOMContentLoaded', generateBoard);
+function randomPerson() {
+    console.log('Inspeccioname esta.')
+    const randomNumber = Math.floor(Math.random() * 19);
+    selectedPerson = characters[randomNumber];
+}
+
+document.addEventListener('DOMContentLoaded', randomPerson);
